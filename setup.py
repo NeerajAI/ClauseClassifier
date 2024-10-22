37b90d45b35422defbf85ebcafb07150f59b439a
NeerajAI <apple@Apples-MacBook-Pro.local>
2024-10-22 23:28:25 +0530

    requirements included

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+REPO_NAME= "ClauseClassifier" # github repository name
+AUTHOR_USER_NAME="Neeraj"
+SRC_REPO="clause_classifier" # project name 
+AUTHOR_EMAIL="Neerajyds@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for natural language processing tasks related to clauses.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages = setuptools.find_packages(where="src")
+)
